827f063bc06072a93e04d3dcbba3a38278682a7a
INTRANET\fca <fernando.andrade@pitang.com>
2017-12-12 18:40:50 -0200

    todo para execução do método de sdk da plataforma

--- worker/run.js
+++ worker/run.js
@@ -4,7 +4,8 @@ const args = process.argv;
 
 var instprocess = process.argv[2];
 
-// TODO recuperar o context da memória de processamento.
+// TODO 1. recuperar o context da memória de processamento.
+// TODO 2. obter do metadado (crud) o arquivo e função que espera esse evento, pelo nome do evento.
 
 
 console.log(instprocess);
