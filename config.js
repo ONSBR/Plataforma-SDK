dd417b4beea6e159d9b54cafa56a3344c5720b30
INTRANET\fca <fernando.andrade@pitang.com>
2017-12-15 01:10:07 -0200

    merge

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ var config = {};
 config.PORT = 8081;
 config.eventManagerUrl = "http://localhost:8081/event";
 config.processMemoryUrl = "http://localhost:9091/";
+config.domainAppUrl = "http://localhost:9090/app1/persist";
 
 module.exports = config;
