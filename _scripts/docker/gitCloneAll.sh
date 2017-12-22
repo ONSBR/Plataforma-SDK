e74d7dabec45e5ca5dd2348e6241c560e0783d05
Hugo Menezes <hugomenezes@gmail.com>
2017-12-22 15:43:33 -0300

    script para clonar os repositórios

---
+++ _scripts/docker/gitCloneAll.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ../../..
+echo "Clonando repositórios"
+git clone git@github.com:ONSBR/Plataforma-EventManager.git
+git clone git@github.com:ONSBR/Plataforma-Executor.git
+git clone git@github.com:ONSBR/Plataforma-PresentationApp.git
+git clone git@github.com:ONSBR/Plataforma-ProcessApp.git
+#git clone git@github.com:ONSBR/Plataforma-SDK.git
+git clone git@github.com:ONSBR/Plataforma-Router.git
+git clone git@github.com:ONSBR/Plataforma-core.git
+git clone git@github.com:ONSBR/Plataforma-ProcessMemory.git
+git clone git@github.com:ONSBR/Plataforma-Domain.git
