81a45b7b369b7564712383aa72b063c552a9de9c
Hugo Menezes <hugomenezes@gmail.com>
2017-12-28 14:56:02 -0300

    teste

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 var config = {};
 
+const eventManagerHost = process.env.EVENT_MANAGER_HOST || "localhost";
+
 config.PORT = 8081;
-config.eventManagerUrl = "http://localhost:8081/event";
+config.eventManagerUrl = "http://" + eventManagerHost + ":8081/event";
 config.processMemoryUrl = "http://localhost:9091/";
 config.domainAppUrl = "http://localhost:9090/";
 
