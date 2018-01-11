42ffdd9096e43544f1df98514ae3192aa65bcea1
Rodrigo Canellas <rodrigo.canellas@gmail.com>
2018-01-11 11:46:22 -0200

    Classe 'operation' para inserção e recuperação de operações

--- services/api-core/operation.js
+++ services/api-core/operation.js
@@ -1,25 +1,86 @@
 
 var Creator = require("./creator");
+var Finder = require("./finder");
 
 module.exports = class Operation {
+
+    /** Creates a 'Operation' object
+     * 
+     * @param {*} configuration is a JSON with this structure:
+     * @code
+        {
+            scheme: scheme, 
+            host:host, 
+            port:port
+        }
+     *
+     * @example
+        {
+            scheme: 'http', 
+            host: 'localhost', 
+            port: '9100'
+        } 
+     */    
     constructor(configuration){
         this.creator = new Creator(configuration);
+        this.finder = new Finder(configuration);
     }
 
-/*
-[
-    {
-        "processId": "995d5970-1c56-483f-bf3c-90fdcbc428b7",
-        "method": "realizeTransferencia",
-        "file":"index.js",
-        "_metadata": {
-            "type": "operation",
-            "changeTrack":"create"
+    /**
+     * 
+     * @param {*} operation is a JSON with this structure:
+     * @code
+        {
+            "processId": process-id,
+            "method": process-method,
+            "file": process-file,           
         }
-    }
-]
-*/
+     * 
+     * @example
+            var scheme = "http"
+            var host = "localhost";
+            var port = "9100";
 
+
+            const Process = require("../api-core/process")
+            const System = require("../api-core/system")
+            const Operation = require("../api-core/operation")
+
+            var sysService = new System({scheme: scheme, host:host, port:port});
+            var procService = new Process({scheme: scheme, host:host, port:port});
+            var operService = new Operation({scheme: scheme, host:host, port:port});
+
+            var systemData = {
+                "name": "bank",
+                "description": "A Testing Bank App",
+                "version":"0.0.4",
+            }
+
+            sysService.create(systemData).then((systemId) => { 
+                console.log("system id = ", systemId);
+                var procData = {
+                    "systemId": systemId,
+                    "name": "Transferência",
+                    "relativePath":"./",
+                    "deployDate": new Date(),    
+                }
+                
+                procService.create(procData).then(processId => {
+                    console.log("processId = ", processId);
+
+                    var operData = {
+                        "processId": processId,
+                        "method": "realizeTransferencia",
+                        "file":"index.js",           
+                    }
+                    operService.create(operData).then(operId => {
+                        console.log("operId = ", operId)
+                    });
+                });
+            });
+
+
+     */
     create(operation){
 
         operation._metadata = {
@@ -29,10 +90,59 @@ module.exports = class Operation {
         return this.creator.create([operation]);
     }
 
-    findById(id){}
+    /**
+     * 
+     * @param {*} id is the identifier of the operation
+     * 
+     * @example
+            var scheme = "http"
+            var host = "localhost";
+            var port = "9100";
 
-    findByName(){
-        return this.finder.byName('operation');
+            const Operation = require("../api-core/operation")
+
+            var procOperation = new Operation({scheme: scheme, host:host, port:port});
+
+            procOperation.findById("986c8437-5a25-481d-86c5-9cceec8848a2").then((id) => { 
+                console.log('operation = ', id);
+            });     
+     */
+    findById(id){
+        var criteria = {
+            filterName : "byId",
+            fieldName : "id",
+            fieldValue : id
+        }    
+
+        return this.finder.find('operation', criteria, 1);
+    }
+
+    /**
+     * 
+     * @param {*} processId is the identifier of the process that owns the opetations
+     * 
+     * @example
+
+        var scheme = "http"
+        var host = "localhost";
+        var port = "9100";
+
+        const Operation = require("../api-core/operation")
+
+        var procOperation = new Operation({scheme: scheme, host:host, port:port});
+
+        procOperation.findByProcessId("6e4c0d35-99a0-4262-9715-f7bfb1d25e09").then((list) => { 
+            console.log('operations = ', list);
+        });
+     */
+    findByProcessId(processId){
+        var criteria = {
+            filterName : "byProcessId",
+            fieldName : "processId",
+            fieldValue : processId
+        }    
+
+        return this.finder.find('operation', criteria);
     }    
 
 }
