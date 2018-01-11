e7fd2867fbc63c21a24290a138f3cb21e2d05540
Philippe Moneda <philippemoneda@gmail.com>
2018-01-11 16:14:50 -0200

    volta os metodos especificos do operation instance

--- services/api-core/operationInstance.js
+++ services/api-core/operationInstance.js
@@ -6,4 +6,32 @@ module.exports = class OperationInstance extends BaseEntity {
     constructor(configuration){
         super(configuration,"operationInstance");
     }
+
+    findByProcessInstanceId(processInstanceId){
+        var criteria = {
+            filterName : "byProcessInstanceId",
+            parameters :
+            [
+                {
+                    fieldName : "processInstanceId",
+                    fieldValue : processInstanceId
+                }
+            ]
+        }
+        return this.finder.find('operationInstance', criteria);
+    }
+
+    findByOperationId(operationId){
+        var criteria = {
+            filterName : "byOperationId",
+            parameters :
+            [
+                {
+                    fieldName : "operationId",
+                    fieldValue : operationId
+                }
+            ]
+        }
+        return this.finder.find('operationInstance', criteria);
+    }
 }
