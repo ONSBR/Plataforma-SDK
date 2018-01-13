a453a7e40224366300a139bc1680d37c50cfface
Philippe Moneda <philippemoneda@gmail.com>
2018-01-13 11:34:52 -0200

    adiciona installedApp no client

---
+++ services/api-core/installedApp.js
@@ -0,0 +1,27 @@
+const Creator = require("./creator");
+const Finder = require("./finder");
+const BaseEntity = require("./baseEntity");
+
+module.exports = class InstalledApp extends BaseEntity {
+    constructor(configuration){
+        super(configuration,"installedApp");
+    }
+
+    findBySystemIdType(systemId,type){
+        var criteria = {
+            filterName : "bySystemIdAndType",
+            parameters :
+            [
+                {
+                    fieldName : "systemId",
+                    fieldValue : systemId
+                },
+                {
+                    fieldName : "type",
+                    fieldValue : type
+                }
+            ]
+        }
+        return this.finder.find('installedApp', criteria);
+    }
+}
