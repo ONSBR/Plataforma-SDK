50d5c9b7d2f5ad8ddd5454524ec29a049357f7cb
Philippe Moneda <philippemoneda@gmail.com>
2018-02-27 15:41:44 -0300

    adiciona suporte para busca de filtros manuais

--- services/process-memory/client.js
+++ services/process-memory/client.js
@@ -7,16 +7,19 @@ module.exports = class ProcessMemoryClient{
     commit(context){
         var d = this.env.processMemory;
         var url = `${d.scheme}://${d.host}:${d.port}/${context.instanceId}/commit`;
+        console.log(`posting to process memory ${JSON.stringify(context,null,4)}`);
         return this.http.post(url,context);
     }
 
     head(instanceId){
+        console.log(`get head of process memory ${instanceId}`);
         var d = this.env.processMemory;
         var url = `${d.scheme}://${d.host}:${d.port}/${instanceId}/head`;
         return this.http.get(url);
     }
 
     first(context){
+        console.log(`get first of process memory ${context.instanceId}`)
         var d = this.env.processMemory;
         var url = `${d.scheme}://${d.host}:${d.port}/${context.instanceId}/first`;
         return this.http.get(url);
