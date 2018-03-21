18214531abd03e0947af19f229e9a2606e52b8ba
Philippe Moneda <philippemoneda@gmail.com>
2018-03-21 15:09:51 -0300

    ajustes no SDK

--- worker/sdk.js
+++ worker/sdk.js
@@ -7,20 +7,39 @@ lookup["domainClient"],
 lookup["processMemory"],
 lookup["eventManager"]);
 
-module.exports = {
-    run: (entryPoint) => {
-        app.start(entryPoint).catch(e => {
-            console.log(e);
-        });
-    },
 
-    mount:(mountingPoint)=>{
-        if (typeof mountingPoint !== "function"){
-            console.log("mountingPoint expected: Function");
-            console.log("got:");
-            console.log(mountingPoint);
-            return;
+const Resolver = {
+    serve:(entryPoint)=>{
+        if (typeof entryPoint === "function" && process.env.API_MODE == true){
+            entryPoint();
+        }else{
+            throw new Error("entrypoint should be a function");
         }
-        app.loadDataFromDomain = mountingPoint;
     }
 };
+
+module.exports = (function() {
+    var self = {
+        run: (entryPoint) => {
+            app.start(entryPoint).catch(e => {
+                console.log(e);
+            });
+        },
+
+        mount:(mountingPoint)=>{
+            if (typeof mountingPoint !== "function"){
+                console.log("mountingPoint expected: Function");
+                console.log("got:");
+                console.log(mountingPoint);
+                return;
+            }
+            app.loadDataFromDomain = mountingPoint;
+        },
+
+        bind:(dispatcher)=>{
+            self.dispatcher = dispatcher;
+            return Resolver;
+        }
+    };
+    return self;
+})();
