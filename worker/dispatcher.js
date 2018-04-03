e30189829e2ec4e93193df7045888a7518fc3baf
Philippe Moneda <philippemoneda@gmail.com>
2018-04-03 14:56:10 -0300

    ajuste

--- worker/dispatcher.js
+++ worker/dispatcher.js
@@ -1,43 +1,48 @@
 const ProcessApp = require("./ProcessApp");
 const Lookup = require("../ioc/lookup");
 let lookup = new Lookup();
-const app = new ProcessApp(lookup["info"],
-lookup["coreFacade"],
-lookup["domainClient"],
-lookup["processMemory"],
-lookup["eventManager"]);
 
 
 
-module.exports = class Dispatcher{
-    constructor(){
+
+module.exports = class Dispatcher {
+    constructor() {
         this.listeners = {};
         this.lastCount = 0;
     }
-    register(eventName, callback){
+    register(eventName, callback) {
         this.listeners[eventName] = callback;
     }
 
 
-    dispatch(event, payload){
+    dispatch(event, payload) {
         console.log(event);
-        var exeEvent ={
+        var exeEvent = {
             name: event,
             appOrigin: "sdk_node",
             payload: payload
         };
         var executor = lookup["executor"];
         var eventManager = lookup["eventManager"];
+        var processInstance = {};
         return executor.createInstance(exeEvent).then(instance => {
             console.log("instance created");
+            processInstance = instance;
             exeEvent.instanceId = instance.id;
             return eventManager.save(exeEvent);
-        }).then(()=>{
+        }).then(() => {
+            var info = {
+                "systemId": processInstance.systemId,
+                "processInstanceId": processInstance.id,
+                "processId": processInstance.processId
+            };
+            const app = new ProcessApp(info,
+                lookup["coreFacade"],
+                lookup["domainClient"],
+                lookup["processMemory"],
+                lookup["eventManager"]);
             console.log("event saved on event store");
-            return new Promise((ok,error)=>{
-                console.log("execute entrypoint");
-                this.listeners[event](scope,ok,error);
-            });
+            return app.start(this.listeners[event]);
         });
     }
 };
