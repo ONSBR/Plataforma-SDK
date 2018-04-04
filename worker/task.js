f97fcd544e81deabed44b904f0f17605c8b3da62
Philippe Moneda <philippemoneda@gmail.com>
2018-04-04 18:15:34 -0300

    bugfix

--- worker/task.js
+++ worker/task.js
@@ -11,11 +11,12 @@ module.exports = class Task {
     constructor(dispatcher, instanceId){
         this.dispatcher = dispatcher;
         this.instanceId = instanceId;
+        this.processMemory = lookup["processMemory"];
     }
 
     start(){
         return this.processMemory.head(this.instanceId).then(head => {
-            var entrypoint = this.dispatcher.listeners[head.event.name];
+             var entrypoint = this.dispatcher.listeners[head.event.name];
              return app.start(entrypoint);
         });
     }
