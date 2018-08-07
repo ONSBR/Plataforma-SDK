a1519680b85dfc3ecd9363d351e28407709939b0
Philippe Moneda <philippemoneda@gmail.com>
2018-08-07 18:55:22 -0300

    ajustes

--- worker/dispatcher.js
+++ worker/dispatcher.js
@@ -29,28 +29,37 @@ module.exports = class Dispatcher {
         var executor = lookup["executor"];
         var eventManager = lookup["eventManager"];
         var processInstance = {};
-
-        eventManager.save(exeEvent).then(eventCreated => {
-            return executor.createInstance(eventCreated).then(instance => {
-                console.log("instance created");
-                processInstance = instance;
-                exeEvent.instanceId = instance.id;
-                var info = {
-                    "systemId": processInstance.systemId,
-                    "processInstanceId": processInstance.id,
-                    "processId": processInstance.processId,
-                    "eventIn": event,
-                    "persistDomainSync":true
-                };
-                const app = new ProcessApp(info,
-                    lookup["coreFacade"],
-                    lookup["domainClient"],
-                    lookup["processMemory"],
-                    lookup["eventManager"]);
-                console.log("event saved on event store");
-                return app.start(this.listeners[event]);
+        return new Promise((resolve, reject)=>{
+            eventManager.save(exeEvent).then(eventCreated => {
+                console.log(eventCreated)
+                return executor.createInstance(eventCreated).then(instance => {
+                    console.log("instance created");
+                    processInstance = instance;
+                    eventCreated.instanceId = instance.id;
+                    var info = {
+                        "systemId": processInstance.systemId,
+                        "processInstanceId": processInstance.id,
+                        "processId": processInstance.processId,
+                        "eventIn": event,
+                        "persistDomainSync":true
+                    };
+                    const app = new ProcessApp(info,
+                        lookup["coreFacade"],
+                        lookup["domainClient"],
+                        lookup["processMemory"],
+                        lookup["eventManager"]);
+                    console.log("event saved on event store");
+                    return app.start(this.listeners[event]).then(resolve).catch(reject)
+                }).catch(e => {
+                    console.log(e)
+                    reject(e)
+                })
+            }).catch(e => {
+                console.log(e)
+                reject(e)
             })
         })
+
 
     }
 };
